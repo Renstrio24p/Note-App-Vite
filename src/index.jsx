2330c3ff7313d47ac2a25c343a434e0659d43605
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-06-12 15:32:08 +0800

    add all wip

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactApp from "./react-18/react";
+import "https://kit.fontawesome.com/6b203b1712.js";
+import "./index.css";
+import Start from "./start"; 
+
+const DOM = ReactDOM
+DOM.render(
+    <React.StrictMode>
+        <Start />       
+    </React.StrictMode>
+,
+document.getElementById('root'))
+
+
---
+++ src/start.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NoteApp from "./project/Note-App/Notes";
+
+export default function Start() {
+  return (
+    <div>
+      <NoteApp />
+      {/*You can start coding right here*/}
+    </div>
+  );
+}
+
+// this is the same as App.js in CRA
